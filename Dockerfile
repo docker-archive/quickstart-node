3579919f95e7d4d04c3f568d8bf04e74ce13c04f
Siedrix <Siedrix@gmail.com>
2015-02-15 18:03:38 -0600

    Update ro run with out redis and run on port 80

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,8 @@ WORKDIR /app
 RUN npm install
 
 ENV NODE_ENV production
+ENV PORT 80
 
-EXPOSE 3000
+EXPOSE 80
+
 CMD ["node", "server.js"]
--- server.js
+++ server.js
@@ -3,20 +3,35 @@ var app = express();
 var redis = require("redis");
 
 var r;
+var options = {
+	max_attempts : 1
+};
+var redisConnected = false;
+
 if( process.env.QUICKSTARTNODE_REDIS_1_PORT && process.env.QUICKSTARTNODE_REDIS_1_ENV_REDIS_PASS ){
-	var options = {
-		auth_pass : process.env.QUICKSTARTNODE_REDIS_1_ENV_REDIS_PASS
-	};
+	options.auth_pass = process.env.QUICKSTARTNODE_REDIS_1_ENV_REDIS_PASS;
 
 	var port = process.env.QUICKSTARTNODE_REDIS_1_PORT_6379_TCP_PORT;
 	var host = process.env.QUICKSTARTNODE_REDIS_1_PORT_6379_TCP_ADDR;
 
 	r = redis.createClient(port, host, options);
 }else{
-	r = redis.createClient();
+	r = redis.createClient(null, null, options);
 }
 
+r.on('error', function(err){
+	console.log('Error on redis', err);
+});
+
+r.on('connect', function(){
+	redisConnected = true;
+});
+
 app.get('/', function (req, res) {
+	if(!redisConnected){
+		return res.send('No conection to redis');
+	}
+
 	r.incr('counter', function (err, data) {
 		if(err){return res.status(500).send(err);}
 
@@ -28,7 +43,7 @@ app.get('/env', function (req, res) {
 	res.json( process.env );
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
 	var host = server.address().address;
 	var port = server.address().port;
 
