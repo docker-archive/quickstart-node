46d3108130393c48d599a24c6f0f15380c90c8db
Borja Burgos <borja@tutum.co>
2015-02-21 17:51:27 -0500

    lots of changes (see details)
    - change from env var links to hostname links (env var links suck)
    - attempted support for redis with no password
    - better resemble other quick start (eventually want to standardize all
    of them)
    - changed /index.html and log output

--- server.js
+++ server.js
@@ -1,51 +1,62 @@
 var express = require('express');
 var app = express();
 var redis = require("redis");
+var os = require("os");
+var time = require('time');
 
 var r;
+var counter;
 var options = {
-	max_attempts : 1
+	max_attempts : null
 };
 var redisConnected = false;
 
-if( process.env.QUICKSTARTNODE_REDIS_1_PORT && process.env.QUICKSTARTNODE_REDIS_1_ENV_REDIS_PASS ){
-	options.auth_pass = process.env.QUICKSTARTNODE_REDIS_1_ENV_REDIS_PASS;
+var port = 6379;
+var host = 'redis';
 
-	var port = process.env.QUICKSTARTNODE_REDIS_1_PORT_6379_TCP_PORT;
-	var host = process.env.QUICKSTARTNODE_REDIS_1_PORT_6379_TCP_ADDR;
-
-	r = redis.createClient(port, host, options);
-}else{
-	r = redis.createClient(null, null, options);
+if (process.env.REDIS_ENV_REDIS_PASS != '**None**') {
+	options.auth_pass = process.env.REDIS_ENV_REDIS_PASS;
 }
 
-r.on('error', function(err){
-	console.log('Error on redis', err);
+r = redis.createClient(port, host, options);
+
+// Check if Redis is connected
+r.on('error', function (err){
+	// console.log('Redis not found or failed to connect: ', err);
+	redisConnected = false;
 });
 
 r.on('connect', function(){
+	console.log('Connected to Redis');
 	redisConnected = true;
 });
 
 app.get('/', function (req, res) {
-	if(!redisConnected){
-		return res.send('No conection to redis');
-	}
 
-	r.incr('counter', function (err, data) {
+	// Variable for Redis to keep track of visit counter
+	console.log('%s handled HTTP REQUEST on %s', os.hostname(), time.Date());
+
+	if(redisConnected){
+		r.incr('counter', function (err, data) {
 		if(err){return res.status(500).send(err);}
+		counter = data;
+		console.log('Visit counter %s', counter)
+		});
+	}else{
+		counter = 'Counter disabled. Redis unavailable or not found.';
+		console.log(counter);
+	}	
 
-		res.send('Visits '+ data);
-	});
+	res.send('Hello ' + process.env.NAME + '!</br> <b>Hostname: </b>' + os.hostname() + '<br/><b>Visits: </b>' + counter);
 });
 
+// Display all environment variables for this container
 app.get('/env', function (req, res) {
 	res.json( process.env );
 });
 
-var server = app.listen(process.env.PORT || 3000, function () {
+var server = app.listen(80, function () {
 	var host = server.address().address;
 	var port = server.address().port;
-
-	console.log('Example app listening at http://%s:%s', host, port);
+	console.log('Listening on port 80 for requests...');
 });
